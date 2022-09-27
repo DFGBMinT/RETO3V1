e7e0cb6f8a46af65e0ca1cdeb89350ff953cb883
DFGBMinT <113607008+DFGBMinT@users.noreply.github.com>
2022-09-27 12:12:58 -0500

    First commit

---
+++ src/main/java/com/example/demo/Modelo/Message.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo.Modelo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author elpro
+ */
+
+@Entity
+@Table(name = "message")
+
+public class Message {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private Integer idMessage;
+    private String messagetext;
+
+    @ManyToOne
+    @JoinColumn(name="carId")
+    @JsonIgnoreProperties({"messages","reservations"})
+    private Car car;
+    
+    @ManyToOne
+    @JoinColumn(name="clientId")
+    @JsonIgnoreProperties({"messages","reservations"})
+    private Client client;
+
+    public Integer getIdMessage() {
+        return idMessage;
+    }
+
+    public void setIdMessage(Integer idMessage) {
+        this.idMessage = idMessage;
+    }
+
+    public String getMessagetext() {
+        return messagetext;
+    }
+
+    public void setMessagetext(String messagetext) {
+        this.messagetext = messagetext;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+    
+    
+}
---
+++ src/main/java/com/example/demo/Ret03G1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ret03G1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ret03G1Application.class, args);
+	}
+
+}
